0ec169dc36c7ec8e4554152cf2075aec78554003
Bola Naguib <bolanaguib@gmail.com>
2019-12-01 02:58:10 +0200

    fixed Multiple displays

--- js/main.js
+++ js/main.js
@@ -1,20 +1,18 @@
 async function supportsWebp() {
   if (!self.createImageBitmap) return false;
-  
-  const webpData = 'data:image/webp;base64,UklGRh4AAABXRUJQVlA4TBEAAAAvAAAAAAfQ//73v/+BiOh/AAA=';
+    const webpData = 'data:image/webp;base64,UklGRh4AAABXRUJQVlA4TBEAAAAvAAAAAAfQ//73v/+BiOh/AAA=';
   const blob = await fetch(webpData).then(r => r.blob());
   return createImageBitmap(blob).then(() => true, () => false);
 }
-
 (async () => {
   if(await supportsWebp()) {
     console.log('does support');
     const jpgclass = $('.jpgclass');
-    jpgclass.hide();
+    jpgclass.show();
   }
   else {
     console.log('does not support');
     const webpclass = $('.webpclass');
-    webpclass.hide();
+    webpclass.show();
   }
 })();
