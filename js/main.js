154f0f6aaf6f1e4f411390a57ede9534c75d909d
Bola Naguib <bolanaguib@gmail.com>
2019-12-01 02:42:36 +0200

    Fixed webp

--- js/main.js
+++ js/main.js
@@ -1,1 +1,20 @@
-console.log('hi');
+async function supportsWebp() {
+  if (!self.createImageBitmap) return false;
+  
+  const webpData = 'data:image/webp;base64,UklGRh4AAABXRUJQVlA4TBEAAAAvAAAAAAfQ//73v/+BiOh/AAA=';
+  const blob = await fetch(webpData).then(r => r.blob());
+  return createImageBitmap(blob).then(() => true, () => false);
+}
+
+(async () => {
+  if(await supportsWebp()) {
+    console.log('does support');
+    const jpgclass = $('.jpgclass');
+    jpgclass.hide();
+  }
+  else {
+    console.log('does not support');
+    const webpclass = $('.webpclass');
+    webpclass.hide();
+  }
+})();
