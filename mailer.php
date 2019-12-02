577f6142342e406d97a6b9146781769d43427001
Your Name <you@example.com>
2019-12-02 19:17:22 +0200

    mm

--- mailer.php
+++ mailer.php
@@ -6,7 +6,6 @@
         $name = strip_tags(trim($_POST["name"]));
 				$name = str_replace(array("\r","\n"),array(" "," "),$name);
         $email = filter_var(trim($_POST["email"]), FILTER_SANITIZE_EMAIL);
-        $budget = trim($_POST["budget"])
         $message = trim($_POST["message"]);
 
         // Check that data was sent to the mailer.
@@ -27,7 +26,6 @@
         // Build the email content.
         $email_content = "Name: $name\n";
         $email_content .= "Email: $email\n\n";
-        $email_content .= "Budget:$budget\n\n";
         $email_content .= "Message:\n$message\n";
 
         // Build the email headers.
@@ -41,7 +39,7 @@
         } else {
             // Set a 500 (internal server error) response code.
             http_response_code(500);
-            echo "Oops! Something went wrong and we couldn't send your message. xxx";
+            echo "Oops! Something went wrong and we couldn't send your message.";
         }
 
     } else {
